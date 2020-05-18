d32b60a87b21206847330e2811aee0a5b0e74aa2
Matt Hall <matt@larvalabs.com>
2020-05-18 01:03:21 -0400

    Switching back to custom URL download.

--- bin/common.sh
+++ bin/common.sh
@@ -38,19 +38,15 @@ download_play_official() {
   local playVersion=${1}
   local playTarFile=${2}
   local playZipFile="play-${playVersion}.zip"
-  #https://github.com/larvalabs/play1/releases/download/1.5.3.larva/play-1.5.3.larva.zip
-  #https://github.com/larvalabs/play1/releases/download/1.5.3.larva/play-1.5.3.larva.zip
-  local playUrl="https://github.com/larvalabs/play1/releases/download/${playVersion}/${playZipFile}"
+  local playUrl="https://downloads.typesafe.com/play/${playVersion}/${playZipFile}"
 
-  echo "Downloading custom play build from ${playUrl}..."
-
-  status=$(curl --retry 3 --silent -w %{http_code} -L ${playUrl} -O)
+  status=$(curl --retry 3 --silent --head -w %{http_code} -L ${playUrl} -o /dev/null)
   if [ "$status" != "200" ]; then
     error "Could not locate: ${playUrl}
-Please check that the version ${playVersion} is correct in your conf/dependencies.yml - status ${status}"
+Please check that the version ${playVersion} is correct in your conf/dependencies.yml"
     exit 1
   else
-    echo "Downloaded ${playZipFile} from Github releases." | indent
+    echo "Downloading ${playZipFile} from https://downloads.typesafe.com" | indent
     curl --retry 3 -s -O -L ${playUrl}
   fi
 
@@ -108,17 +104,18 @@ https://devcenter.heroku.com/articles/scala-support"
 install_play()
 {
   VER_TO_INSTALL=$1
-  PLAY_URL="https://s3.amazonaws.com/heroku-jvm-langpack-play/play-heroku-$VER_TO_INSTALL.tar.gz"
+  PLAY_URL="https://github.com/larvalabs/play1/releases/download/1.5.3.larva/play-1.5.x-1.5.3.larva.tar.gz"
   PLAY_TAR_FILE="play-heroku.tar.gz"
 
   validate_play_version ${VER_TO_INSTALL}
 
   echo "-----> Installing Play! $VER_TO_INSTALL....."
 
-  status=$(curl --retry 3 --silent --head -w %{http_code} -L ${PLAY_URL} -o /dev/null)
+  status=$(curl --retry 3 --max-time 150 --silent -w %{http_code} -L ${PLAY_URL} -o $PLAY_TAR_FILE)
   if [ "$status" != "200" ]; then
     download_play_official ${VER_TO_INSTALL} ${PLAY_TAR_FILE}
   else
+    echo "-----> Successfully downloaded $VER_TO_INSTALL from Github....."
     curl --retry 3 -s --max-time 150 -L $PLAY_URL -o $PLAY_TAR_FILE
   fi
 
